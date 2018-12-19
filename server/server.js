fb5c86a7c297e17fb567a6e22672be74e8c49e97
laraibidrees <laraibidrees@gmail.com>
2018-12-19 11:06:21 +0500

    Added POST todo routes and refactor mongoose

--- server/server.js
+++ server/server.js
@@ -1,28 +1,26 @@
-const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose}=require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type : String
-  },
-  completed: {
-    type: Boolean
-  },
-  completedAt: {
-    type: Number
-  }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo= new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  })
 });
 
-var newTodo = new Todo({
-  text: 'Do Laundry',
-  completed: false,
-  completedAt: 124
-});
-
-newTodo.save().then((doc) => {
-console.log('Saved Todo', doc);
-}, (e) => {
-  console.log('Failed to save todo');
-});
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+})
